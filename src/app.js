43e62c9aa28390ba4d9ad4c48ee7d78ccfaceddb
Emmanuel ERNEST <ernest.emmanuel@hotmail.fr>
2019-07-16 18:47:21 +0200

    Refacto React.createElement Fix key issues

--- src/app.js
+++ src/app.js
@@ -1,5 +1,15 @@
+const e = React.createElement;
+
+const MessageItemUser = props => {
+  return e('span', { className: 'message-item-user'}, props.from);
+};
+
 const MessageItem = props => {
-  return React.createElement('p', { className: 'app-message'}, 'Hello World');
+  return e('li', { key: 'message-item-' + props.id, className: 'app-message'}, [
+    e(MessageItemUser, { key: 'message-item-user', id: props.id, from: props.from}),
+    e('span', {key: 'message-item-span'}, ' : '),
+    props.text
+  ]);
 };
 
 
@@ -7,16 +17,27 @@ class MessageList extends React.Component {
   constructor() {
     super();
     this.state = {
-      messages: ['toto', 'titi', 'tata'],
+      messages: [
+        {
+          id: 1,
+          from: 'Jean',
+          text: 'Hey, comment ça va',
+          date: new Date(),
+        },
+      ],
     }
   }
 
   render() {
-    return React.createElement('div', null, [
-      React.createElement(MessageItem, null),
+    return e('div', null, [
+      e('ul', { key: 'message-list',  className: 'message-list'}, this.state.messages.map(item => {
+        return e(MessageItem, {key: 'message-list-' + item.id,  id: item.id, text: item.text, from: item.from });
+      })),
+      e('input', { key: 'message-input',  className: 'message-input'}),
+      e('button', { key: 'message-button', className: 'send'}, 'Envoyer'),
     ]);
   }
 }
 
 const app = document.getElementById('app');
-ReactDOM.render(React.createElement(MessageList), document.getElementById('app'));
+ReactDOM.render(e(MessageList), document.getElementById('app'));
